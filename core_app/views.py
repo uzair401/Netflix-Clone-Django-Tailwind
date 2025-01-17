8aea61566faa6e2ea8c5892b91a87da01416d4ba
uzair <developeruzair401@gmail.com>
2025-01-17 18:06:07 +0500

    Dashboard Design

--- core_app/views.py
+++ core_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello, World!")
+    return render(request, 'home.html')
 
 def movies(request):
     return HttpResponse("Movies Page")
